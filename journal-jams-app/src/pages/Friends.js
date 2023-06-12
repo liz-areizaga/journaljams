db8d348625ec4826920eb3442f579eba38a9409b
liz-areizaga <larei002@ucr.edu>
2023-06-12 15:38:04 -0700

    Implemented commenting on Friends's entries
    Implemented commenting and upvote/downvote on Friends page. Needs real time upvote/downvote.

--- journal-jams-app/src/pages/Friends.js
+++ journal-jams-app/src/pages/Friends.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState, useContext} from 'react';
 import './Pages.css';
 import NavBar from "../Components/NewNavbar/Navbar";
-import { Box, Button, Typography, List, ListItemButton, ListItemText} from '@mui/material';import Modal from 'react-modal';
+import { Box, Button, Typography, List, ListItemButton, ListItemText, TextField, ListItem, InputLabel } from '@mui/material';import Modal from 'react-modal';
 import SearchBar from "../Components/Searchbar/Searchbar";
 import { UserContext } from "../contexts/user.context";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import IconButton from '@mui/material/IconButton'
+import Stack from '@mui/material/Stack'
 
 const Friends = () => {  
 
@@ -14,6 +18,8 @@ const Friends = () => {
   const [isEntryOpen, setIsEntryOpen] = useState(false);  
   const [data, setData] = useState([]);
   const [currentUser, setCurrentUser] = useState("");
+  const [comments, setComments] = useState([]) 
+  const [isCommenting, setIsCommenting] = useState(false);
   const [selectedFriend, setSelectedFriend] = useState("");
   const [friendBirthday, setFriendBirthday] = useState("");
   const [friendAboutMe, setFriendAboutMe] = useState("");  
@@ -30,6 +36,28 @@ const Friends = () => {
 
   var templist = [];
   var tempSelectedFriend = "";
+
+  const increase = (entry_id, index) => {
+    fetch(`/api/upVote/${entry_id}/${index}`, { method: "PUT" })
+      .then((response) => {
+        if (response.ok) {
+          console.log('Comment updated successfully');
+        } else {
+          throw new Error('Failed to update comment');
+        }
+      })
+  };
+  
+  const decrease = (entry_id, index) => {
+    fetch(`/api/downVote/${entry_id}/${index}`, { method: "PUT" })
+    .then((response) => {
+      if (response.ok) {
+        console.log('Comment updated successfully');
+      } else {
+        throw new Error('Failed to update comment');
+      }
+    })
+  };
 
   const handleFetchUser = async () => {
     try {
@@ -51,6 +79,18 @@ const Friends = () => {
       console.error("Error fetching user:", error);
     }
   };
+
+  const handleFetchComments = async (entry) => {
+    try {
+      fetch(`/api/allComments/${entry}`, { method: "GET" }).then(response=>response.json())
+        .then((jsonRes) => {
+          setComments(jsonRes);
+        })
+    } catch (err) {
+      console.log("Error in handleFechComments")
+      console.log(err);
+    }
+  }
 
   useEffect(() => () => { handleFetchUser();}, []);
 
@@ -99,6 +139,8 @@ const Friends = () => {
 
   const handleEntryClick = (entry) => {
     setSelectedEntry(entry);
+    setComments([]);
+    handleFetchComments(entry._id);
     setIsEntryOpen(true);
   };
 
@@ -147,6 +189,39 @@ const Friends = () => {
 
   function toggleFriends() {
     setShowFriends(!showFriends);
+  }
+
+  const addComment = (event) => {
+    console.log(selectedEntry)
+    event.preventDefault();
+    fetch('/api/newComment', {
+      method: 'POST',
+      body: JSON.stringify({username: currentUser, entry_id: selectedEntry._id, comment: document.getElementById('comment').value, rating: 0}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((response) => {
+      if (response.ok) {
+        console.log('Comment submitted successfully');
+        // Handle the response from the API
+      } else {
+        throw new Error('Failed to submit comment');
+      }
+    })
+    .catch((error) => {
+      console.error('Error submitting comment:', error);
+      alert(error);
+    })
+    setComments([...comments, {user: currentUser, comment: document.getElementById('comment').value, rating: '0'}])
+    document.getElementById('comment').value = "";
+    setIsCommenting(false);
+    // handleFetchComments(selectedEntry.id);
+    // window.location.reload();
+  }
+
+  const handleComment = () => {
+    setIsCommenting(!isCommenting);
   }
 
   return (
@@ -267,6 +342,50 @@ const Friends = () => {
                         <Typography variant="body1" gutterBottom> Text: {selectedEntry.text} </Typography>
                       </>
                     )}
+                    <Box>
+                    { comments.length > 0 && (comments.map((comment, i) => (
+                      <List key={i}>
+                        <ListItem sx={{border:"1px solid black", borderRadius: "30px"}} >
+                        <ListItemText
+                        primary={
+                          <Typography variant="h6" style={{ fontSize: '16px' }}>
+                            {comment.user}
+                          </Typography>
+                        }
+                        secondary={
+                          <Typography variant="h5" style={{ fontSize: '20px' }}>
+                            {comment.comment}
+                          </Typography>
+                        }
+                        />
+                          <Box id = "arrow_container" style={{marginTop:'12px'}}>
+                          <Stack direction="column" spacing = {2}>
+                              <IconButton id = "arrows" onClick = {() => {increase(selectedEntry._id, i)}}>
+                                  <KeyboardArrowUpIcon/> 
+                              </IconButton>
+                              <span id="vote" >{comment.rating}</span>
+                              <IconButton id = "arrows" onClick = {() => {decrease(selectedEntry._id, i)}}>
+                                  <KeyboardArrowDownIcon /> 
+                              </IconButton>
+                          </Stack>
+                      </Box>
+                        </ListItem>
+                      </List>
+                    )))}
+                    </Box>
+                    { isCommenting && 
+                        (<form onSubmit={addComment}>
+                          <Box mb={2}>
+                            <InputLabel className="modal-labels" htmlFor="exampleInputPassword1">Comment</InputLabel>
+                            <TextField id="comment" name="comment" variant="outlined" fullWidth />
+                          </Box> 
+                          <Box display="flex" justifyContent="flex-end">
+                            <Button variant="contained" color="error" style={{ marginTop: '10px', marginRight: '10px' }} onClick={handleComment}>Close</Button>
+                            <Button variant="contained" style={{ marginTop: '10px', marginRight: '10px' }} type="submit" >Submit</Button>
+                          </Box>
+                        </form> 
+                      )}
+                      <Button variant="contained" style={{ marginTop: '10px', marginRight: '10px' }} onClick={handleComment}>Comment</Button>
                     <Button variant="contained" color="error" style={{ marginTop: '10px', marginRight: '10px' }} onClick={closeEntryModal}>Close</Button>
                   </Modal>  
                 </Box>
